3e91db5b7d47502310786e1e44d1d83e6a638590
Jordan Mor <jordanmor@gmail.com>
2018-08-30 19:40:39 -0400

    Render new loan page

--- routes/loans.js
+++ routes/loans.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const Loans = require('../models').Loans;
 const Books = require('../models').Books;
+const Patrons = require('../models').Patrons;
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
+const moment = require('moment');
 
 // GET all loans / overdue loans / checked out loans
 router.get('/', (req, res) => {
@@ -55,7 +57,26 @@ router.get('/', (req, res) => {
 });
 
 router.get('/new', (req, res) => {
-  res.render('loans/new');
+
+  const books = Books.findAll();
+  const patrons = Patrons.findAll();
+  Promise.all([books, patrons])
+    .then( results => {
+      const now = moment().format('YYYY-MM-DD');
+      const nextWeek = moment().add(7, 'days').format('YYYY-MM-DD');
+      res.render('loans/new', {
+        books: results[0],
+        patrons: results[1],
+        date: now,
+        nextWeek: nextWeek
+      });
+    })
+    .catch(err => res.sendStatus(500));
+});
+
+router.post('/new', (req, res) => {
+  console.log(req.body);
+  res.redirect('/loans');
 });
 
 router.get('/return', (req, res) => {
