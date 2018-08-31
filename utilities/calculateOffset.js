ba8fa0f0d68990b8b623a814d628ba1e0cc9eefc
Jordan Mor <jordanmor@gmail.com>
2018-08-31 18:26:04 -0400

    Consolidate pagination functions into one module

--- utilities/calculateOffset.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(pageNum, pageLimit) {
-  let offset = 0;
-  if(pageNum) {
-    if(pageNum != 1) {
-      offset = (pageNum - 1) * pageLimit;
-    }
-  }
-  return offset;
-}
--- utilities/paginate.js
+++ utilities/paginate.js
@@ -1,8 +1,30 @@
-module.exports = function(totalItems, pageLimit) {
+function paginate(totalItems, pageLimit) {
   const pages = Math.ceil(totalItems / pageLimit);
   let pagesArray = [];
   for(let x = 1; x <= pages; x++) {
-    pagesArray.push(x);
+    pagesArray.push({number: x});
   }
   return pagesArray;
 };
+
+function setActivePage(pages, activePage) {
+  if (!activePage) {
+    pages[0].active = true;
+  } else {
+    pages[activePage - 1].active = true;
+  }
+}
+
+function setOffset(pageNum, pageLimit) {
+  let offset = 0;
+  if(pageNum) {
+    if(pageNum != 1) {
+      offset = (pageNum - 1) * pageLimit;
+    }
+  }
+  return offset;
+}
+
+module.exports.paginate = paginate;
+module.exports.setActivePage = setActivePage;
+module.exports.setOffset = setOffset;
