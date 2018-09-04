4c5c6645ffa454311696f2126277c3112450fd6a
Jordan Mor <jordanmor@gmail.com>
2018-09-04 14:45:23 -0400

    Using jQuery, add an error handler for searches with no input

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,36 @@
 $( document ).ready(function() {
+
+  // Active links
   const regex = /^\/[a-z]*/;
   const basePathname = window.location.pathname.match(regex)[0];
   // Active main nav link
   $(`#main-nav a[href="${basePathname}"]`).addClass('active-link');
   // Active filter link on books and loans page
   $(`#filters a[href="${window.location.pathname}"]`).addClass('active-link');
+
+  // Error handler for search function
+  $('form.search').on('submit', function(e) {
+    
+    if ($('form.search select').val() === '' || $('#searchInput').val() === '') {
+      $('.search-errors').remove();
+      let html = '<div class="search-errors"><p class="errors">';
+
+      if ($('form.search select').val() === '' && $('#searchInput').val() === '') {
+        e.preventDefault();
+        html += 'Please select a keyword and enter a search query'; 
+        $('#searchInput').addClass('errors-bd');
+      } else if ($('form.search select').val() === '') {
+        e.preventDefault();
+        html += 'Please select a keyword';
+      } else if ($('#searchInput').val() === '') {
+        e.preventDefault();
+        html += 'Please enter a search query'; 
+        $('#searchInput').addClass('errors-bd');
+      }
+
+      html += '</p></div>';
+      $('form .search-container').before(html);
+    }
+  });
+
 });
